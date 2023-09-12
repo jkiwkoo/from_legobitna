08d14e5266a14aa29f2e94bf7f6895fddda0132c
jkiwkoo <jkiwkoo@naver.com>
2023-09-12 14:16:25 +0900

    class01

---
+++ js/class01.js
@@ -0,0 +1,25 @@
+const color = 'pink';
+let color2 = 'haha';
+console.log(color, color2);
+
+const num = '123';
+console.log(num);
+
+let password = 1;
+
+password = password % 2; // 나머지
+password = password > 2; // boolean 값 : false
+console.log(password);
+
+// || 는 둘중 하나만 true 여도 true
+// &&는 둘 다 true 여야먄 true
+
+let a = 1;
+let b = 2;
+console.log(a, b);
+
+let c = a;
+a = b;
+b = c;
+
+console.log(a, b);
